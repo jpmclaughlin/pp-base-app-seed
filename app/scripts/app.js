8a99dda84a0cc7fb352275df1d62df80cdd45740
jpmclaughlin <jasonpmclaughlin@gmail.com>
2014-11-10 14:13:20 -0600

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ppBaseAppSeedApp
+ * @description
+ * # ppBaseAppSeedApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ppBaseAppSeedApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ppBaseAppSeedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ppBaseAppSeedApp
+ */
+angular.module('ppBaseAppSeedApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
